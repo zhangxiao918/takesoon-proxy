e7030cd2fb3647e56c45ff59a7c2241ff5db9c88
bluestome <bluestome@163.com>
2018-05-05 02:07:52 +0800

    补充不必要的包

--- proxy-core/src/main/java/com/takesoon/proxy/core/cookie/CookieImpl.java
+++ proxy-core/src/main/java/com/takesoon/proxy/core/cookie/CookieImpl.java
@@ -1,18 +1,18 @@
 package com.takesoon.proxy.core.cookie;
 
-import com.alibaba.fastjson.JSON;
 import com.ouer.cache.redis.RedisCacheService;
 import okhttp3.Cookie;
 import okhttp3.CookieJar;
 import okhttp3.HttpUrl;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.Proxy;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -26,7 +26,9 @@ public class CookieImpl implements CookieJar {
     private Proxy proxy;
     private String tag;
     public static final String CACHE_KEY = "%s:%s";
-    public static final String CACHE_SPEC_KEY = "%s_%s";
+    public static final String PROXY_HOST_CACHE_KEY = "%s:host:%s";
+    public static final String PROXY_COOKIE_DOMAIN_CACHE_KEY_V2 = "%s:cookie:%s";
+    public static final String PROXY_COOKIE_VALUE_CACHE_KEY_V2 = "%s:cookie:%s:%s";
 
     private volatile ConcurrentHashMap<String, ConcurrentHashMap<String, String>> memoryCookies = new ConcurrentHashMap<>();
 
@@ -41,19 +43,21 @@ public class CookieImpl implements CookieJar {
 
     @Override
     public synchronized void saveFromResponse(HttpUrl httpUrl, List<Cookie> cookies) {
-        logger.debug("set-cookie", JSON.toJSONString(cookies));
-        String key = key(String.valueOf(hashCode()), httpUrl.host());
+        String key = String.format(PROXY_HOST_CACHE_KEY, String.valueOf(hashCode()), httpUrl.host());
         if (StringUtils.isNotBlank(tag)) {
-            key = key(tag, httpUrl.host());
+            key = String.format(PROXY_HOST_CACHE_KEY, tag, httpUrl.host());
         }
         for (Cookie cookie : cookies) {
             if (StringUtils.isNotBlank(cookie.name()) && null != cookie) {
                 if (cookie.expiresAt() > now()) {
-                    cacheService.sadd(key, cookie.domain());
-                    cacheService.sadd(cookie.domain(), cookie.name());
-                    String dck = String.format(CACHE_SPEC_KEY, cookie.domain(), cookie.name());
-                    cacheService.put(dck, cookie.toString());
-                    cacheService.expireAfter(dck, (int) (cookie.expiresAt() - System.currentTimeMillis()));
+                    String key2 = String.format(PROXY_COOKIE_DOMAIN_CACHE_KEY_V2, tag, cookie.domain());
+                    cacheService.sadd(key, key2);
+
+                    String key3 = String.format(PROXY_COOKIE_VALUE_CACHE_KEY_V2, tag, cookie.domain(), cookie.name());
+                    cacheService.sadd(key2, key3);
+                    cacheService.put(key3, cookie.toString());
+                    cacheService.expireAfter(key3, (int) ((cookie.expiresAt() - System.currentTimeMillis()) / 1000) - 5);
+
                 }
             }
         }
@@ -63,44 +67,46 @@ public class CookieImpl implements CookieJar {
     @Override
     public List<Cookie> loadForRequest(HttpUrl httpUrl) {
         List<Cookie> list = new ArrayList<Cookie>();
+        boolean existsExpiredCookies = false;
         //从host中获取对应的cookie.domain列表
-        String key = key(String.valueOf(hashCode()), httpUrl.host());
+        String key = String.format(PROXY_HOST_CACHE_KEY, String.valueOf(hashCode()), httpUrl.host());
         if (StringUtils.isNotBlank(tag)) {
-            key = key(tag, httpUrl.host());
+            key = String.format(PROXY_HOST_CACHE_KEY, tag, httpUrl.host());
         }
-        Set<String> tmpCookies = new HashSet<>();
-        Set<String> domains = cacheService.smembers(key);
-        for (String domain : domains) {
-            Set<String> cacheCookieNames = cacheService.smembers(domain);
-            for (String cacheCookieName : cacheCookieNames) {
-                String dck = String.format(CACHE_SPEC_KEY, domain, cacheCookieName);
-                String cacheCookie = cacheService.get(dck, String.class, null);
-                if (StringUtils.isNotBlank(cacheCookie)) {
-                    Cookie cookie = Cookie.parse(httpUrl, cacheCookie);
-                    if (null != cookie) {
-                        if (cookie.expiresAt() > now()) {
-                            list.add(cookie);
-                            tmpCookies.add(cookie.toString());
+        Set<String> cookieDomains = cacheService.smembers(key);
+        if (CollectionUtils.isNotEmpty(cookieDomains)) {
+            for (String cookieDomain : cookieDomains) {
+                if (existsExpiredCookies) {
+                    cacheService.remove(cookieDomain);
+                } else {
+                    Set<String> cookieNames = cacheService.smembers(cookieDomain);
+                    if (CollectionUtils.isNotEmpty(cookieNames)) {
+                        for (String cookieName : cookieNames) {
+                            if (existsExpiredCookies) {
+                                cacheService.remove(cookieName);
+                            } else {
+                                String cacheCookie = cacheService.get(cookieName, String.class, null);
+                                if (StringUtils.isNotBlank(cacheCookie)) {
+                                    Cookie cookie = Cookie.parse(httpUrl, cacheCookie);
+                                    if (null != cookie) {
+                                        if (cookie.expiresAt() > now()) {
+                                            list.add(cookie);
+                                        } else {
+                                            existsExpiredCookies = true;
+                                            cacheService.remove(cookieName);
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
         }
-        if (CollectionUtils.isNotEmpty(tmpCookies)) {
-            logger.debug("loadForRequest.tmpCookies {}", JSON.toJSONString(tmpCookies));
+        if (existsExpiredCookies) {
+            cacheService.remove(key);
         }
         return list;
-    }
-
-    /**
-     * 生成缓存key
-     *
-     * @param proxyServer 代理服务
-     * @param domain
-     * @return
-     */
-    private String key(String proxyServer, String domain) {
-        return String.format(CACHE_KEY, proxyServer, domain);
     }
 
     /**
